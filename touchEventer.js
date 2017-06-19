44ae69e6285c6d39188a4196854201bd8b26180b
Jsung <sanjsun@outlook.com>
2017-06-19 23:59:11 +0800

    新增上滑和下滑事件
    新增上滑和下滑事件、代码优化

--- touchEventer.js
+++ touchEventer.js
@@ -1,72 +1,107 @@
-function leftSwiper(ele, callBack) {
-    var startX = 0;
-    var moveX = 0;
-    ele.addEventListener("touchstart", function (e) {
-        startX = e.touches[0].clientX;
-    })
-    ele.addEventListener("touchmove", function (e) {
-        moveX = e.touches[0].clientX;
+//      向上滑动
+        function topSwiper(ele, callBack) {
+            var startY = 0;
+            var moveY = 0;
+            ele.addEventListener("touchstart", function (e) {
+                startY = e.touches[0].clientY;
+            })
+            ele.addEventListener("touchmove", function (e) {
+                moveY = e.touches[0].clientY;
+            })
+            ele.addEventListener("touchend", function () {
+                if ((moveY - startY) < 0 && moveY != 0) {
+                    callBack()
+                }
+                moveY = 0;
+            })
+        };
+//      向下滑动
+        function bottomSwiper(ele, callBack) {
+            var startY = 0;
+            var moveY = 0;
+            ele.addEventListener("touchstart", function (e) {
+                startY = e.touches[0].clientY;
+            })
+            ele.addEventListener("touchmove", function (e) {
+                moveY = e.touches[0].clientY;
+            })
+            ele.addEventListener("touchend", function () {
+                if ((moveY - startY) > 0 && moveY != 0) {
+                    callBack()
+                }
+                moveY = 0;
+            })
+        };
+        //      向左滑动事件
 
-    })
-    ele.addEventListener("touchend", function () {
-        if ((moveX - startX) < 0 && moveX != 0) {
-            callBack()
+        function leftSwiper(ele, callBack) {
+            var startX = 0;
+            var moveX = 0;
+            ele.addEventListener("touchstart", function (e) {
+                startX = e.touches[0].clientX;
+            })
+            ele.addEventListener("touchmove", function (e) {
+                moveX = e.touches[0].clientX;
+
+            })
+            ele.addEventListener("touchend", function () {
+                if ((moveX - startX) < 0 && moveX != 0) {
+                    callBack()
+                }
+                moveX = 0;
+            })
+        };
+        //      向右滑动事件
+        function rightSwiper(ele, callBack) {
+            var startX = 0;
+            var moveX = 0;
+            ele.addEventListener("touchstart", function (e) {
+                startX = e.touches[0].clientX;
+            })
+            ele.addEventListener("touchmove", function (e) {
+                moveX = e.touches[0].clientX;
+            })
+            ele.addEventListener("touchend", function (e) {
+                if ((moveX - startX) > 0 && moveX != 0) {
+                    (callBack())
+                }
+                moveX = 0;
+            })
         }
-        moveX = 0;
-    })
-};
-//      向右滑动事件
-function rightSwiper(ele, callBack) {
-    var startX = 0;
-    var moveX = 0;
-    ele.addEventListener("touchstart", function (e) {
-        startX = e.touches[0].clientX;
-    })
-    ele.addEventListener("touchmove", function (e) {
-        moveX = e.touches[0].clientX;
-    })
-    ele.addEventListener("touchend", function (e) {
-        if ((moveX - startX) > 0 && moveX != 0) {
-            (callBack())
+        //   长按事件
+        function longPress(ele, callBack) {
+            var startDate = 0;
+            var endDate = 0;
+            var flang = true;
+            ele.addEventListener("touchstart", function (e) {
+                flang = true;
+                startDate = Date.now();
+            })
+            ele.addEventListener("touchmove", function (e) {
+                flang = false;
+            })
+            ele.addEventListener("touchend", function (e) {
+                endDate = Date.now()
+                if (flang && (endDate - startDate) > 300) {
+                    callBack()
+                }
+            })
         }
-        moveX = 0;
-    })
-}
-//   长按事件
-function longPress(ele, callBack) {
-    var startDate = 0;
-    var endDate = 0;
-    var flang = true;
-    ele.addEventListener("touchstart", function (e) {
-        flang = true;
-        startDate = Date.now();
-    })
-    ele.addEventListener("touchmove", function (e) {
-        flang = false;
-    })
-    ele.addEventListener("touchend", function (e) {
-        endDate = Date.now()
-        if (flang && (endDate - startDate) > 300) {
-            callBack()
+        //  双击事件
+        function dbClick(ele, callBack) {
+            var endDate = 0;
+            var dbDate = 0;
+            var index = 0
+            ele.addEventListener("touchend", function (e) {
+                endDate = Date.now();
+                index++;
+                dbDate = endDate - dbDate;
+                if (index >= 2 && dbDate <= 300) {
+                    callBack();
+                }
+                if (index >= 2) {
+                    dbDate = 0;
+                    index = 0;
+                }
+            })
         }
-    })
-}
-//  双击事件
-function dbClick(ele, callBack) {
-    var endDate = 0;
-    var dbDate = 0;
-    var index = 0
-    ele.addEventListener("touchend", function (e) {
-        endDate = Date.now();
-        index++;
-        dbDate = endDate - dbDate;
-        if (index >= 2 && dbDate <= 300) {
-            index = 0;
-            callBack();
-        }
-        if (index >= 2) {
-            dbDate = 0;
-            index = 0;
-        }
-    })
-}
